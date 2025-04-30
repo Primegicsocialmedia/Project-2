bea5e8cfa3b2eda96f26ad2f3d2f7769d1c29708
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-04-30 15:04:44 +0200

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-deepblue p-12'>
+      <h1 className="text-5xl font-bold text-white">This is the template with TailwindCSS</h1>
+      <button className='bg-amber text-alabaster text-2xl font-bold mt-12 rounded px-6 py-4'>Click here !</button>
+    </div>
+  )
+}
+
+export default App
