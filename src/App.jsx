0735f5ebbec5110314ede4792a526d71ede5e7ca
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-05-05 17:08:21 +0200

    add pet form complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,29 @@ import Footer from "./components/Footer";
 import Pet from "./components/Pet";
 import { useState } from "react";
 import LikeArea from "./components/LikeArea";
+import AddPetForm from "./components/AddPetForm";
 
 
 
-const pets = [
-  { name: "Meowsalot", species: "cat", age: "5", id: 123456789 },
-  { name: "Barksalot", species: "dog", age: "3", id: 987654321 },
-  { name: "Fluffy", species: "rabbit", age: "2", id: 123123123 },
-  { name: "Purrsloud", species: "cat", age: "1", id: 456456456 },
-  { name: "Paws", species: "dog", age: "6", id: 789789789 }
-]
+
 
 const App = () => {
+
+
+  const [pets,setPets]= useState([
+    { name: "Meowsalot", species: "cat", age: "5", id: 123456789 },
+    { name: "Barksalot", species: "dog", age: "3", id: 987654321 },
+    { name: "Fluffy", species: "rabbit", age: "2", id: 123123123 },
+    { name: "Purrsloud", species: "cat", age: "1", id: 456456456 },
+    { name: "Paws", species: "dog", age: "6", id: 789789789 }
+  ])
+
   return (
     <>
       <Header />
       <LikeArea />
       <TimeArea />
-      
+      <AddPetForm setPets={setPets}/>
       <ul>
         {pets.map((pet) => (
           <Pet
---
+++ src/components/AddPetForm.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const AddPetForm = (props) => {
+  const [name, setName] = useState();
+  const [species, setSpecies] = useState();
+  const [age, setAge] = useState();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.setPets((prev) =>
+      prev.concat({ name, species, age, id: Date.now() })
+    );
+
+    setName("");
+    setSpecies("");
+    setAge("");
+  }
+  return (
+    <form onSubmit={handleSubmit} className="m-2 p-2">
+      <fieldset>
+        <legend className="p-2 font-bold">Add New Pet</legend>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Name"
+        />
+        <input
+          value={species}
+          onChange={(e) => setSpecies(e.target.value)}
+          placeholder="species"
+        />
+        <input
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+          placeholder="age in years"
+        />
+        <button className="border-1 font-bold p-2">Add Pet</button>
+      </fieldset>
+    </form>
+  );
+};
+
+export default AddPetForm;
